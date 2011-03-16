29966e79de8531cd1f60bc26de5c671043cc52d9
Jeremy Pfund <jpfund@wgen.net>
2011-03-16 11:34:32 -0400

    Make ci build script more configurable (cross envs/hostclass)

--- scripts/build/ci_pk12_build.sh
+++ scripts/build/ci_pk12_build.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
-app=$1
-gitrepo=$2
+hostclass=$1
+app=$2
+gitrepo=$3
+env=$4
 
 # remove the code RPM
-/opt/wgen/funcdeploy/wg_funcdeploy.py -e $FUNC_ENV mhcttwebapp remove mclass-tt-$app
+/opt/wgen/funcdeploy/wg_funcdeploy.py -e $FUNC_ENV $hostclass remove mclass-tt-$app
 
 # build the RPM
 rm -rf $WORKSPACE/RPM_STAGING
 mkdir -p $WORKSPACE/opt/tt/webapps/$app
 python /opt/wgen/rpmtools/wg_rpmbuild.py -v -o $BUILD_RPM_REPO -r $WORKSPACE/RPM_STAGING -D${app}dir=$WORKSPACE -Drpm_version=$RPM_VERSION -Dbuildnumber=$BUILD_NUMBER $WORKSPACE/rpm/tt-$app.spec
 
-# update rpm repo
+# update rpm repo$hostclass
 /opt/wgen/rpmtools/wg_createrepo $BUILD_RPM_REPO
 
 # deploy the rpm should update configs and install rpms.
-/opt/wgen/funcdeploy/wg_funcdeploy.py -e $FUNC_ENV mhcttwebapp install mclass-tt-$app
-/opt/wgen/funcdeploy/wg_funcdeploy.py -e $FUNC_ENV mhcttwebapp bcfgcliupdate
+/opt/wgen/funcdeploy/wg_funcdeploy.py -e $FUNC_ENV $hostclass install mclass-tt-$app
+/opt/wgen/funcdeploy/wg_funcdeploy.py -e $FUNC_ENV $hostclass bcfgcliupdate
 
 # start the webapp
-/opt/wgen/funcdeploy/wg_funcdeploy.py -e $FUNC_ENV mhcttwebapp service tt-web-app start
+/opt/wgen/funcdeploy/wg_funcdeploy.py -e $FUNC_ENV $hostclass service tt-web-app start
 
 # run webdriver tests on remote box
-echo $JOB_NAME $ENV_PROPERTY_PREFIX $BUILD_TAG $BUILD_BRANCH "true" | ssh -i /home/tomcat/.ssh/autobuild_key autobuild@yad124.tt.wgenhq.net /home/autobuild/devel/3_12/$app-$app-future-ci/ci-webdriver.sh
+echo $JOB_NAME $ENV_PROPERTY_PREFIX $BUILD_TAG $BUILD_BRANCH "true" | ssh -i /home/tomcat/.ssh/autobuild_key autobuild@yad124.tt.wgenhq.net /home/autobuild/devel/3_12/$app-$app-$env-ci/ci-webdriver.sh
 
 # build migration rpms to QA repo and move code rpm to the same
 cp $BUILD_RPM_REPO/mclass-tt-$app-$RPM_VERSION-$BUILD_NUMBER.noarch.rpm $NEXT_RPM_REPO
