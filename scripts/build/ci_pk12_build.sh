71a83d977b4e0582fc17dba0b5df38c60ede3799
Dan Fichter <dfichter@wgen.net>
2011-03-21 18:47:56 -0400

    temp revert 40e3eef09 and set -e

--- scripts/build/ci_pk12_build.sh
+++ scripts/build/ci_pk12_build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 hostclass=$1
 app=$2
 gitrepo=$3
@@ -32,3 +34,8 @@ python /opt/wgen/rpmtools/wg_rpmbuild.py -v -o $NEXT_RPM_REPO -r $WORKSPACE/RPM_
 
 # update QA rpm repo
 /opt/wgen/rpmtools/wg_createrepo $NEXT_RPM_REPO
+
+## if build is successful, move the last-stable branch to the current commit
+## (this should always be the last step in the build)
+git branch -f last-stable-$BUILD_BRANCH
+git push -f git@mcgit.mc.wgenhq.net:312/$gitrepo.git last-stable-$BUILD_BRANCH
