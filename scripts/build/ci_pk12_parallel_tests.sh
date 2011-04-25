7453bd28f0a023bc40b4fd920efbe638e4def94e
Dan Fichter <dfichter@wgen.net>
2011-04-25 18:25:27 -0400

    run unit tests on slaves

--- scripts/build/ci_pk12_parallel_tests.sh
+++ scripts/build/ci_pk12_parallel_tests.sh
@@ -8,6 +8,6 @@ set -eu
 apphomeenvvar=$1 # e.g., OUTCOMES_HOME
 testsperbatch=$2 # e.g., 8, to farm 8 tests to each slave at a time
 
-find target/test/integration target/test/webservice -name *Test.class \
+find target/test/unit target/test/integration target/test/webservice -name *Test.class \
   | xargs -i basename {} .class \
   | python conf/base/scripts/build/parallelTests.py -s yad127.tt.wgenhq.net,yad128.tt.wgenhq.net,yad129.tt.wgenhq.net -u autobuild -i /home/tomcat/.ssh/autobuild_key -w /home/autobuild/$JOB_NAME -v $apphomeenvvar -n $testsperbatch
