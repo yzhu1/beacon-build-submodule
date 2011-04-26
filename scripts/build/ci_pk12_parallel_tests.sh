d8e18befd02ae31aecb29f5db0d58cd82d24ec12
Dan Fichter <dfichter@wgen.net>
2011-04-25 20:19:53 -0400

    move wgspringcore integration tests back into main target

--- scripts/build/ci_pk12_parallel_tests.sh
+++ scripts/build/ci_pk12_parallel_tests.sh
@@ -9,8 +9,6 @@ apphomeenvvar=$1 # e.g., OUTCOMES_HOME
 testsperbatch=$2 # e.g., 8, to farm 8 tests to each slave at a time
 
 # Find all unit, integration, webservice and wgspringcore integration test names and pipe them to parallelTests.py
-(   find ivy_lib/compile -name *wgspringcore*integration*jar -exec jar -tf \{} \; \
- && find target/test/unit target/test/integration target/test/webservice \
-) | grep Test.class \
+find target/test/unit target/test/integration target/test/webservice -name *Test.class \
   | xargs -i basename {} .class \
   | python conf/base/scripts/build/parallelTests.py -s yad127.tt.wgenhq.net,yad128.tt.wgenhq.net,yad129.tt.wgenhq.net -u autobuild -i /home/tomcat/.ssh/autobuild_key -w /home/autobuild/$JOB_NAME -v $apphomeenvvar -n $testsperbatch
