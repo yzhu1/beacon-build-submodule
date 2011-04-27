eea84564b6c1210792ddec681f39896af1634e11
Dan Fichter <dfichter@wgen.net>
2011-04-27 06:30:07 -0400

    run unit tests firsts, then all other tests

--- scripts/build/ci_pk12_parallel_tests.sh
+++ scripts/build/ci_pk12_parallel_tests.sh
@@ -7,7 +7,12 @@ set -eu
 apphomeenvvar=$1 # e.g., OUTCOMES_HOME
 testsperbatch=$2 # e.g., 8, to farm 8 tests to each slave at a time
 
-# Find all unit, integration, webservice and wgspringcore integration test names and pipe them to parallelTests.py
-find target/test/unit target/test/integration target/test/webservice -name *Test.class \
+# Copy the workspace to the slaves, and run unit tests
+find target/test/unit -name *Test.class \
   | xargs -i basename {} .class \
-  | python conf/base/scripts/build/parallelTests.py -s yad127.tt.wgenhq.net,yad128.tt.wgenhq.net,yad129.tt.wgenhq.net -u autobuild -i /home/tomcat/.ssh/autobuild_key -w /home/autobuild/$JOB_NAME -v $apphomeenvvar -n $testsperbatch
+  | python conf/base/scripts/build/parallelTests.py -s yad127.tt.wgenhq.net,yad128.tt.wgenhq.net,yad129.tt.wgenhq.net -u autobuild -i /home/tomcat/.ssh/autobuild_key -w /home/autobuild/$JOB_NAME -v $apphomeenvvar -n $testsperbatch -c
+
+# Run db updates on the slaves and then all integration and webservice tests
+find target/test/integration target/test/webservice -name *Test.class \
+  | xargs -i basename {} .class \
+  | python conf/base/scripts/build/parallelTests.py -s yad127.tt.wgenhq.net,yad128.tt.wgenhq.net,yad129.tt.wgenhq.net -u autobuild -i /home/tomcat/.ssh/autobuild_key -w /home/autobuild/$JOB_NAME -v $apphomeenvvar -n $testsperbatch -d
