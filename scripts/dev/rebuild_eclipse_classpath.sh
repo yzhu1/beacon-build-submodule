13de8af64c87664ccd237573b61597fc33cabdfb
Dan Fichter <dfichter@wgen.net>
2011-05-07 08:11:08 -0400

    script cleanup

--- scripts/dev/rebuild_eclipse_classpath.sh
+++ scripts/dev/rebuild_eclipse_classpath.sh
@@ -11,6 +11,34 @@ set -e
 
 echo 'rebuilding eclipse classpath'
 
+classpathline() {
+  echo "$1" >> .classpath
+}
+
+# Write out head of .classpath
+echo "<?xml version='1.0' encoding='UTF-8'?>" > .classpath
+classpathline "<classpath>"
+classpathline "    <classpathentry kind='src' path='src/main/java'/>"
+classpathline "    <classpathentry including='*-context.xml' kind='src' path='src/test/resources'/>"
+classpathline "    <classpathentry kind='src' path='target/web-assets/unzip/wgspringcore-web-assets/web/WEB-INF'/>"
+classpathline "    <classpathentry kind='src' path='target/web-assets/unzip/wgspringcore-web-assets/resources'/>"
+classpathline "    <classpathentry excluding='java/' kind='src' path='src/test/webdriver/util'/>"
+classpathline "    <classpathentry excluding='java/' kind='src' path='src/test/integration/util'/>"
+classpathline "    <classpathentry kind='src' path='src/main/webapp/WEB-INF'/>"
+classpathline "    <classpathentry kind='src' path='src/main/resources'/>"
+classpathline "    <classpathentry kind='src' path='src/test/java'/>"
+classpathline "    <classpathentry kind='src' path='src/test/integration/controller/java'/>"
+classpathline "    <classpathentry kind='src' path='src/test/integration/service/java'/>"
+classpathline "    <classpathentry kind='src' path='src/test/integration/util/java'/>"
+classpathline "    <classpathentry kind='src' path='src/test/webdriver/java'/>"
+classpathline "    <classpathentry kind='src' path='src/test/webdriver/util/java'/>"
+classpathline "    <classpathentry kind='src' path='src/test/webservice/util/java'/>"
+classpathline "    <classpathentry kind='src' path='src/test/webservice/java'/>"
+classpathline "    <classpathentry kind='con' path='org.eclipse.jst.j2ee.internal.web.container'/>"
+classpathline "    <classpathentry kind='con' path='org.eclipse.jst.j2ee.internal.module.container'/>"
+classpathline "    <classpathentry kind='con' path='org.eclipse.jdt.launching.JRE_CONTAINER'/>"
+classpathline "    <classpathentry kind='output' path='target/eclipse/classes'/>"
+
 # Find all unique jars under ivy_lib/
 jars=`find ivy_lib -name *.jar|xargs -i basename {}|sort|uniq`
 
@@ -22,34 +50,6 @@ for jar in $jars; do
   fi
 done
 
-classpathentry() {
-  echo "$1" >> .classpath
-}
-
-# Write out head of .classpath
-echo "<?xml version='1.0' encoding='UTF-8'?>" > .classpath
-classpathentry "<classpath>"
-classpathentry "	<classpathentry kind='src' path='src/main/java'/>"
-classpathentry "	<classpathentry including='*-context.xml' kind='src' path='src/test/resources'/>"
-classpathentry "	<classpathentry kind='src' path='target/web-assets/unzip/wgspringcore-web-assets/web/WEB-INF'/>"
-classpathentry "	<classpathentry kind='src' path='target/web-assets/unzip/wgspringcore-web-assets/resources'/>"
-classpathentry "	<classpathentry excluding='java/' kind='src' path='src/test/webdriver/util'/>"
-classpathentry "	<classpathentry excluding='java/' kind='src' path='src/test/integration/util'/>"
-classpathentry "	<classpathentry kind='src' path='src/main/webapp/WEB-INF'/>"
-classpathentry "	<classpathentry kind='src' path='src/main/resources'/>"
-classpathentry "	<classpathentry kind='src' path='src/test/java'/>"
-classpathentry "	<classpathentry kind='src' path='src/test/integration/controller/java'/>"
-classpathentry "	<classpathentry kind='src' path='src/test/integration/service/java'/>"
-classpathentry "	<classpathentry kind='src' path='src/test/integration/util/java'/>"
-classpathentry "	<classpathentry kind='src' path='src/test/webdriver/java'/>"
-classpathentry "	<classpathentry kind='src' path='src/test/webdriver/util/java'/>"
-classpathentry "	<classpathentry kind='src' path='src/test/webservice/util/java'/>"
-classpathentry "	<classpathentry kind='src' path='src/test/webservice/java'/>"
-classpathentry "	<classpathentry kind='con' path='org.eclipse.jst.j2ee.internal.web.container'/>"
-classpathentry "	<classpathentry kind='con' path='org.eclipse.jst.j2ee.internal.module.container'/>"
-classpathentry "	<classpathentry kind='con' path='org.eclipse.jdt.launching.JRE_CONTAINER'/>"
-classpathentry "	<classpathentry kind='output' path='target/eclipse/classes'/>"
-
 # Write out ivy entries in .classpath
 for jar in $jars; do
   if [[ $bin_jars_with_src_available != *$jar* ]]; then # Don't create dup entries for bin jars for which we have sources
@@ -60,11 +60,11 @@ for jar in $jars; do
     fi
     if [[ $jar == *src* ]]; then # If this is a source jar, write out an entry with both bin and src
       bin=`echo $jar|sed 's/-src//'`
-      classpathentry "    <classpathentry kind='lib' path='$bin' sourcepath='$jar' />"
+      classpathline "    <classpathentry kind='lib' path='$bin' sourcepath='$jar' />"
     else                         # Otherwise, write out a regular entry
-      classpathentry "    <classpathentry kind='lib' path='$jar' />"
+      classpathline "    <classpathentry kind='lib' path='$jar' />"
     fi
   fi
 done
 
-classpathentry '</classpath>'
+classpathline '</classpath>'
