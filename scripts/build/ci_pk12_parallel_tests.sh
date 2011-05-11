1e3267b9a3c77e2ced6850e7a8e13661dbe122f9
Dan Fichter <dfichter@wgen.net>
2011-05-11 13:30:35 -0400

    bring back yad127

--- scripts/build/ci_pk12_parallel_tests.sh
+++ scripts/build/ci_pk12_parallel_tests.sh
@@ -11,7 +11,7 @@ testsperbatch=$2 # e.g., 8, to farm 8 tests to each testdog at a time
 echo "RUNNING UNIT TESTS IN PARALLEL"
 find target/test/unit -name *Test.class \
   | xargs -i basename {} .class \
-  | /opt/wgen-3p/python26/bin/python conf/base/scripts/build/parallelTests.py -s yad128.tt.wgenhq.net,yad129.tt.wgenhq.net -u autobuild -i /home/jenkins/.ssh/autobuild_key -w /home/autobuild/$JOB_NAME -v $apphomeenvvar -n $testsperbatch -c
+  | /opt/wgen-3p/python26/bin/python conf/base/scripts/build/parallelTests.py -s yad127.tt.wgenhq.net,yad128.tt.wgenhq.net,yad129.tt.wgenhq.net -u autobuild -i /home/jenkins/.ssh/autobuild_key -w /home/autobuild/$JOB_NAME -v $apphomeenvvar -n $testsperbatch -c
 
 # Run db updates on the testdogs and then all integration and webservice tests
 echo "RUNNING INTEGRATION AND WEBSERVICE TESTS IN PARALLEL"
@@ -19,4 +19,4 @@ echo "RUNNING INTEGRATION AND WEBSERVICE TESTS IN PARALLEL"
  && find target/test/integration target/test/webservice \
 ) | grep Test.class \
   | xargs -i basename {} .class \
-  | /opt/wgen-3p/python26/bin/python conf/base/scripts/build/parallelTests.py -s yad128.tt.wgenhq.net,yad129.tt.wgenhq.net -u autobuild -i /home/jenkins/.ssh/autobuild_key -w /home/autobuild/$JOB_NAME -v $apphomeenvvar -n $testsperbatch -d
+  | /opt/wgen-3p/python26/bin/python conf/base/scripts/build/parallelTests.py -s yad127.tt.wgenhq.net,yad128.tt.wgenhq.net,yad129.tt.wgenhq.net -u autobuild -i /home/jenkins/.ssh/autobuild_key -w /home/autobuild/$JOB_NAME -v $apphomeenvvar -n $testsperbatch -d
