0d78e2f3e0674fa396a332c433d059ff9f26b411
Dan Fichter <dfichter@wgen.net>
2011-06-02 11:41:12 -0400

    make wgspringcore integration tests optional in build script

--- scripts/build/ci_pk12_parallel_tests.sh
+++ scripts/build/ci_pk12_parallel_tests.sh
@@ -2,10 +2,11 @@
 
 # See parallelTests.py for more details.
 
-set -eu
+set -eux
 
 apphomeenvvar=$1 # e.g., OUTCOMES_HOME
 testsperbatch=$2 # e.g., 8, to farm 8 tests to each testdog at a time
+runwgspringcoreintegrationtests=$3 # projects with no-op internal entity repos can't run these, see tech debt #362
 
 # Copy the workspace to the testdogs, and run unit tests
 echo "RUNNING UNIT TESTS IN PARALLEL"
@@ -15,7 +16,20 @@ find target/test/unit -name *Test.class \
 
 # Run db updates on the testdogs and then all integration and webservice tests
 echo "RUNNING INTEGRATION AND WEBSERVICE TESTS IN PARALLEL"
-( find target/test/integration target/test/webservice \
-) | grep Test.class \
-  | xargs -i basename {} .class \
-  | /opt/wgen-3p/python26/bin/python conf/base/scripts/build/parallelTests.py -s yad127.tt.wgenhq.net,yad128.tt.wgenhq.net,yad129.tt.wgenhq.net -u autobuild -i /home/jenkins/.ssh/autobuild_key -w /home/autobuild/$JOB_NAME -v $apphomeenvvar -n $testsperbatch -d
+if [ $runwgspringcoreintegrationtests = "y" ]; then
+    (   find ivy_lib/compile -name *wgspringcore*integration*jar -exec jar -tf \{} \; \
+     && find target/test/integration target/test/webservice \
+    ) | grep Test.class \
+      | xargs -i basename {} .class \
+      | /opt/wgen-3p/python26/bin/python conf/base/scripts/build/parallelTests.py \
+        -s yad127.tt.wgenhq.net,yad128.tt.wgenhq.net,yad129.tt.wgenhq.net -u autobuild \
+        -i /home/jenkins/.ssh/autobuild_key -w /home/autobuild/$JOB_NAME -v $apphomeenvvar -n $testsperbatch -d
+else
+    ( find target/test/integration target/test/webservice \
+    ) | grep Test.class \
+      | xargs -i basename {} .class \
+      | /opt/wgen-3p/python26/bin/python conf/base/scripts/build/parallelTests.py \
+        -s yad127.tt.wgenhq.net,yad128.tt.wgenhq.net,yad129.tt.wgenhq.net -u autobuild \
+        -i /home/jenkins/.ssh/autobuild_key -w /home/autobuild/$JOB_NAME -v $apphomeenvvar -n $testsperbatch -d
+
+fi
