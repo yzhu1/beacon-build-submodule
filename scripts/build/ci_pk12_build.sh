755a8a9f76346b0d9ce8c8b5b3a9053bde03eff9
Dan Fichter <dfichter@wgen.net>
2011-06-07 13:54:32 -0400

    fix wgr command

--- scripts/build/ci_pk12_build.sh
+++ scripts/build/ci_pk12_build.sh
@@ -24,7 +24,7 @@ python /opt/wgen/rpmtools/wg_rpmbuild.py -v -o $BUILD_RPM_REPO -r $WORKSPACE/RPM
 /opt/wgen/rpmtools/wg_createrepo $BUILD_RPM_REPO
 
 # deploy and start the webapp
-ssh -i /home/jenkins/.ssh/wgrelease wgrelease@$autoreleasebox /opt/wgen/wgr/bin/wgr.py -r $releaseversion -e $wgrenv -f -s -g \"$webapphostclass\" -a "release_start.sh ${webapphostclass}_stop.sh ${webapphostclass}_rm_rpm.sh ${webapphostclass}_bcfg.sh ${webapphostclass}_start.sh" -v
+ssh -i /home/jenkins/.ssh/wgrelease wgrelease@$autoreleasebox /opt/wgen/wgr/bin/wgr.py -r $releaseversion -e $wgrenv -f -s -g \"$webapphostclass\" -a \"release_start.sh ${webapphostclass}_stop.sh ${webapphostclass}_rm_rpm.sh ${webapphostclass}_bcfg.sh ${webapphostclass}_start.sh\" -v
 
 # run webdriver tests in parallel on testdog
 echo "RUNNING WEBDRIVER TESTS"
