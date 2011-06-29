529c1b6e447110125535a57311a74e056586bdd7
Dan Fichter <dfichter@wgen.net>
2011-06-29 11:12:57 -0400

    add comment

--- scripts/build/check_for_postgres_user_in_db_snap.sh
+++ scripts/build/check_for_postgres_user_in_db_snap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# If this fails, run sed -i 's/postgres/tt_manage/g' sql_snaps/<latest fixtures snap>
+
 set -exu
 snap=$1
 echo "In the baseline and fixtures snapshots, we don't want any references to the postgres user -- should be tt_manage instead -- otherwise ci migrations will fail"
