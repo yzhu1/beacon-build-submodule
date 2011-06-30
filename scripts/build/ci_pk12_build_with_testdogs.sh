361a5d6c625be41f1f5579b78dc702ab8d43fa0c
Dan Fichter <dfichter@wgen.net>
2011-06-29 23:38:27 -0400

    make all build script vars local

--- scripts/build/ci_pk12_build_with_testdogs.sh
+++ scripts/build/ci_pk12_build_with_testdogs.sh
@@ -15,34 +15,43 @@ gitrepo=$GIT_REPO                       # e.g., itembank-web or outcomes
 env=$ENV                                # e.g., futureci or currentci
 migrationsappname=$MIGRATIONS_APP_NAME  # e.g., itembank or outcomes
 autoreleasebox=$AUTORELEASE_BOX         # e.g., tmc142.mc.wgenhq.net (consult cmdb)
+rpmversion=$RPM_VERSION                 # e.g., 13.0.0
 releaseversion=$RELEASE_VERSION         # e.g., mc13.0.0
+buildbranch=$BUILD_BRANCH               # e.g., master
+buildrpmrepo=$BUILD_RPM_REPO            # e.g., $REPO_FUTURE_CI
+nextrpmrepo=$NEXT_RPM_REPO              # e.g., $REPO_FUTURE_QA
+runonlysmoke=$RUN_ONLY_SMOKE            # e.g., true
+
+# Set automatically by Jenkins
+buildtag=$BUILD_TAG
+buildnumber=$BUILD_NUMBER
+workspace=$WORKSPACE
 
 # Set more environment variables
 export ANT_OPTS="-Xms128m -Xmx2048m -XX:MaxPermSize=256m -XX:-UseGCOverheadLimit"
-export RUN_ONLY_SMOKE=true
 export ENV_PROPERTY_PREFIX=testdog${env}0 # Set to testdog0 that we can test up/down migrations on one testdog
 
 # Clean workspace
 rm -rf target
 
-# ivy-resolve first so we can get WGSPRINGCORE_VERSION accurately
+# ivy-resolve first so we can get wgspringcoreversion accurately
 /opt/wgen-3p/ant-1.7.0/bin/ant ivy-resolve
 
 # Tag this build in git.
-git tag -a -f -m "Jenkins Build #$BUILD_NUMBER" $BUILD_TAG
-git push -f git@mcgit.mc.wgenhq.net:312/$gitrepo +refs/tags/$BUILD_TAG:$BUILD_TAG
+git tag -a -f -m "Jenkins Build #$buildnumber" $buildtag
+git push -f git@mcgit.mc.wgenhq.net:312/$gitrepo +refs/tags/$buildtag:$buildtag
 
 # Set properties that'll get templated into basic.ftl
-export GIT_REVISION=`git log -1 --pretty=format:%H`
-export WGSPRINGCORE_VERSION=`python conf/base/scripts/build/getWgspringcoreVersion.py`
-echo "git.common.revision=$WGSPRINGCORE_VERSION" >> conf/build.properties
-echo "git.revision=$GIT_REVISION" >> conf/build.properties
-echo "build.number=$BUILD_NUMBER" >> conf/build.properties
-echo "build.branch=$BUILD_BRANCH" >> conf/build.properties
-echo "rpm.version=$RPM_VERSION" >> conf/build.properties
+gitrevision=`git log -1 --pretty=format:%H`
+wgspringcoreversion=`python conf/base/scripts/build/getWgspringcoreVersion.py`
+echo "git.common.revision=$wgspringcoreversion" >> conf/build.properties
+echo "git.revision=$gitrevision" >> conf/build.properties
+echo "build.number=$buildnumber" >> conf/build.properties
+echo "build.branch=$buildbranch" >> conf/build.properties
+echo "rpm.version=$rpmversion" >> conf/build.properties
 
 # Stop the webapps before messing with their dbs
-ssh -i /home/jenkins/.ssh/wgrelease wgrelease@$AUTORELEASE_BOX /opt/wgen/wgr/bin/wgr.py -r $releaseversion -e $env -f -s -g \"$webapphostclass\" -a \"release_start.sh ${webapphostclass}_stop.sh\"
+ssh -i /home/jenkins/.ssh/wgrelease wgrelease@$autoreleasebox /opt/wgen/wgr/bin/wgr.py -r $releaseversion -e $env -f -s -g \"$webapphostclass\" -a \"release_start.sh ${webapphostclass}_stop.sh\"
 
 # Compile, run static and unit tests, migrate one db up and down
 /opt/wgen-3p/ant-1.7.0/bin/ant clean-deploy checkstyle template-lint jslint test-compile test-unit clear-schema load-baseline-database migrate-schema rollback-schema
@@ -58,20 +67,20 @@ echo "RUNNING INTEGRATION AND WEBSERVICE TESTS IN PARALLEL"
     -v $apphomeenvvar -n $testsperbatch -d
 
 # Build webapp and db rpms
-rm -rf $WORKSPACE/RPM_STAGING
-mkdir -p $WORKSPACE/opt/tt/webapps/$app
-python /opt/wgen/rpmtools/wg_rpmbuild.py -v -o $BUILD_RPM_REPO -r $WORKSPACE/RPM_STAGING -D${app}dir=$WORKSPACE -Drpm_version=$RPM_VERSION -Dbuildnumber=$BUILD_NUMBER $WORKSPACE/rpm/tt-$app.spec
-python /opt/wgen/rpmtools/wg_rpmbuild.py -v -o $BUILD_RPM_REPO -r $WORKSPACE/RPM_STAGING -Dcheckoutroot=$WORKSPACE -Drpm_version=$RPM_VERSION -Dbuildnumber=$BUILD_NUMBER $WORKSPACE/rpm/tt-migrations-$app.spec
+rm -rf $workspace/RPM_STAGING
+mkdir -p $workspace/opt/tt/webapps/$app
+python /opt/wgen/rpmtools/wg_rpmbuild.py -v -o $buildrpmrepo -r $workspace/RPM_STAGING -D${app}dir=$workspace -Drpm_version=$rpmversion -Dbuildnumber=$buildnumber $workspace/rpm/tt-$app.spec
+python /opt/wgen/rpmtools/wg_rpmbuild.py -v -o $buildrpmrepo -r $workspace/RPM_STAGING -Dcheckoutroot=$workspace -Drpm_version=$rpmversion -Dbuildnumber=$buildnumber $workspace/rpm/tt-migrations-$app.spec
 
 # Promote them to CI rpm repo
-/opt/wgen/rpmtools/wg_createrepo $BUILD_RPM_REPO
+/opt/wgen/rpmtools/wg_createrepo $buildrpmrepo
 
 # Deploy webapp, update bcfg, start webapp
 ssh -i /home/jenkins/.ssh/wgrelease wgrelease@$autoreleasebox /opt/wgen/wgr/bin/wgr.py -r $releaseversion -e $env -f -s -g \"$webapphostclass\" -a \"release_start.sh ${webapphostclass}_rm_rpm.sh ${webapphostclass}_bcfg.sh ${webapphostclass}_start.sh\"
 
 # Run webdriver tests in parallel on testdogs, first loading fixture data (-d)
 echo "RUNNING WEBDRIVER TESTS"
-if [ $RUN_ONLY_SMOKE = 'false' ]; then
+if [ $runonlysmoke = 'false' ]; then
     runslowtestsflag='-l'
 else
     runslowtestsflag=
@@ -83,10 +92,10 @@ find target/test/webdriver -name *Test.class \
     -v $apphomeenvvar -n 1000 -d $runslowtestsflag
 
 # All tests have passed!  The build is good!  Promote RPMs to QA RPM repo
-cp $BUILD_RPM_REPO/mclass-tt-$app-$RPM_VERSION-$BUILD_NUMBER.noarch.rpm $NEXT_RPM_REPO
-cp $BUILD_RPM_REPO/tt-migrations-$migrationsappname-$RPM_VERSION-$BUILD_NUMBER.noarch.rpm $NEXT_RPM_REPO
-/opt/wgen/rpmtools/wg_createrepo $NEXT_RPM_REPO
+cp $buildrpmrepo/mclass-tt-$app-$rpmversion-$buildnumber.noarch.rpm $nextrpmrepo
+cp $buildrpmrepo/tt-migrations-$migrationsappname-$rpmversion-$buildnumber.noarch.rpm $nextrpmrepo
+/opt/wgen/rpmtools/wg_createrepo $nextrpmrepo
 
 # Move the last-stable tag to the current commit
-git branch -f last-stable-$BUILD_BRANCH
-git push -f git@mcgit.mc.wgenhq.net:312/$gitrepo.git last-stable-$BUILD_BRANCH
+git branch -f last-stable-$buildbranch
+git push -f git@mcgit.mc.wgenhq.net:312/$gitrepo.git last-stable-$buildbranch
