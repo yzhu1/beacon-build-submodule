1cda1df007b0bc6fadf47eb8a8ad72f2eb79891d
Dan Fichter <dfichter@wgen.net>
2011-07-19 09:32:50 -0400

    move testdog0 back in

--- scripts/build/ci_pk12_build_with_testdogs.sh
+++ scripts/build/ci_pk12_build_with_testdogs.sh
@@ -67,11 +67,8 @@ if [ $isnightlybuild != 'true' ]; then
     ) | grep Test.class \
       | xargs -i basename {} .class \
       | /opt/wgen-3p/python26/bin/python conf/base/scripts/build/parallelTests.py \
-        -s testdog${env}1,testdog${env}2,testdog${env}3 \
+        -s testdog${env}0,testdog${env}1,testdog${env}2,testdog${env}3 \
         -v $apphomeenvvar -n $testsperbatch -d
-
-    # re-migrate testdog0 since we didn't run parallelTests.py -d on it
-    /opt/wgen-3p/ant-1.7.0/bin/ant migrate-schema
 
     # Build webapp and db rpms
     rm -rf $workspace/RPM_STAGING
