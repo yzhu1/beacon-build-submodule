5c67700c25fea38967ae99ebf505220457f79d83
Jesse Greenberg <jgreenberg@wgen.net>
2011-11-23 14:55:40 -0500

    Changed EOL script to change all file types before exiting EOL script now also prints just the file path

--- scripts/windowsEOL.sh
+++ scripts/windowsEOL.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 
 cd ../../../
-
+set error = false;
 if test $(file `find . -name *.java` | grep CRLF | wc -l) -ne 0 
 then
-	echo "The following Java files have windows line endings"
-	file `find . -name *.java` | grep CRLF | sed 's/  */\ /g'
-	exit 1
+	error=true;
 fi
 
 if test $(file `find . -name *.js` | grep CRLF | wc -l) -ne 0 
 then
-	echo "The following javaScript files have windows line endings"
- file `find . -name *.js` | grep CRLF | sed 's/  */\ /g'
-	exit 1
+	error=true;
 fi
 
 if test $(file `find . -name *.scss` | grep CRLF | wc -l) -ne 0 
 then
-	echo "The following scss files have windows line endings"
-	file `find . -name *.scss` | grep CRLF | sed 's/  */\ /g'
-	exit 1
+	error=true;
 fi
 
 if test $(file `find . -name *.ftl` | grep CRLF | wc -l) -ne 0 
 then
-	echo "The following FTL files have windows line endings"
-	file `find . -name *.ftl` | grep CRLF | sed 's/  */\ /g'
-	exit 1
+	error=true;
 fi
-
+if $error ; then
+echo "Files with CRLF instead of LF" 
+  file `find . -name *.java` | grep CRLF | sed 's/  */\ /g' | sed 's/:.*$//'
+  file `find . -name *.js` | grep CRLF | sed 's/  */\ /g'| sed 's/:.*$//'
+  file `find . -name *.scss` | grep CRLF | sed 's/  */\ /g' | sed 's/:.*$//'
+  file `find . -name *.ftl` | grep CRLF | sed 's/  */\ /g' | sed 's/:.*$//'
+  exit 1
+fi
 echo "There are no windows EOLs present"
