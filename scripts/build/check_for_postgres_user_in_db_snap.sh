96cb354b7aa591de70718c40c3ef00cf990bdf59
Ben Warfield <bwarfield@wgen.net>
2011-11-29 12:05:08 -0500

    Shell script tweak: avoid useless echoing of commands, and unnecesary pipe. Also updated explanatory output to be slightly easier to read.

--- scripts/build/check_for_postgres_user_in_db_snap.sh
+++ scripts/build/check_for_postgres_user_in_db_snap.sh
@@ -2,7 +2,9 @@
 
 # If this fails, run sed -i 's/postgres/tt_manage/g' sql_snaps/<latest fixtures snap>
 
-set -exu
+set -eu
 snap=$1
-echo "In the baseline and fixtures snapshots, we don't want any references to the postgres user -- should be tt_manage instead -- otherwise ci migrations will fail"
-exit `grep postgres $snap|wc -l`
+echo "In the baseline and fixtures snapshots, we don't want any references to the postgres user"
+echo "(Should be tt_manage instead, otherwise ci migrations will fail)"
+echo "Checking for instances of 'postgres' in" $snap
+exit `grep -c postgres $snap`
