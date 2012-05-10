8747dd4687b22ee5c05f4d07131f4192d5ab00e9
Ankit Goswami <agoswami@contractor.wgen.net>
2012-05-10 05:18:59 -0400

    removed option to pass in custom diff in favour of passing in multiple commit ids

--- scripts/dev/wikilink.py
+++ scripts/dev/wikilink.py
@@ -14,8 +14,7 @@ topReviewTemplate = ("=Overview=\n"
     +"*Comments Expected By: (datetime)\n"
     +"=Files for Review=")
 
-bottomReviewTemplate = ("=Commits=\n"
-    +"=Fallout=\n"
+bottomReviewTemplate = ("=Fallout=\n"
     +"''Fallout list will be created here after review/discussion.''\n"
     +"=Comments=  \n"
     +"==<Your Name Here>==\n"
@@ -33,14 +32,14 @@ def find_by_name(pattern):
 def getargs():
     parser = optparse.OptionParser()
     parser.add_option("-b", "--branch-files", dest="branchfiles",action="store_true",help="link files that are changed on this branch")
-    parser.add_option("-d", "--custom-diff",type="string", dest="customdiff",help="link files that are changed according to supplied diff")
     parser.add_option("-t", "--add-template",action="store_true", dest="template",help="add code review template to output")
+    parser.add_option("-c", "--commits", action="store_true", dest="commits",help="link files that are changed by given commits")
     parser.set_description(
         "Generate links to files on the appropriate repository browsing application.  By default, "
         + "takes a list of patterns on the command line and generates a link for every file below "
         + "the current directory that matches one of those patterns."
         + "\n\nIf no patterns are given, reads a list of files to generate links for from STDIN."
-        + "\nYou can also pass it a custom diff (in quotes) and it will generate a list of the files included in that diff."
+        + "\nYou can also pass it a list of commits and it will generate links for all the files changed in those commits."
         )
     parser.set_usage("%prog [options] [pattern ... ]")
     return parser.parse_args()
@@ -64,6 +63,15 @@ class wikilinker(object):
         return wikilinker("https://{server}/gerrit/gitweb?p={project_root}.git;a=blob;f={path}",
                           ";hb=refs/heads/{branch}", repo)
 
+    @staticmethod
+    def get_cgit_commit_linker(repo):
+        return wikilinker("http://{server}/cgit/cgit.cgi/{project_root}/commit/?id={path}", "&h={branch}", repo)
+
+    @staticmethod
+    def get_gitweb_commit_linker(repo):
+        return wikilinker("https://{server}/gerrit/gitweb?p={project_root}.git;a=commit;h={path}",
+                          ";hb=refs/heads/{branch}", repo)
+
     def get_link_url(self, path):
         repo = self._repository
         rooted_path = repo.rooted_path(path)
@@ -81,34 +89,48 @@ class wikilinker(object):
         basename = os.path.basename(path)
         return "* [%s %s]" % (url, basename)
 
+    def make_wiki_commit_link(self, commit, msg):
+        url = self.get_link_url(commit)
+        return "* [%s %s]" % (url, msg)
+
 def main():
     (opts, arglist) = getargs()
     r = gitutils.get_repo()
-    if opts.customdiff:
-        if not "--name-only" in opts.customdiff:
-            opts.customdiff += " --name-only"
-        file_list = r.branch_files(opts.customdiff)
-    elif opts.branchfiles:
+    commits = []
+    file_list = []
+    if opts.branchfiles:
         file_list = r.branch_files()
+    elif opts.commits and (0 < len(arglist)):
+        commits = arglist
+        for commit in commits:
+            diff = "git diff " + commit + "^!"
+            file_list = file_list + list(set(r.branch_files(diff)) - set(file_list))
     elif (0 < len(arglist)):
         file_list = []
         for argpath in arglist:
             file_list.extend(find_by_name(argpath))
     else:
         file_list = [filepath.strip() for filepath in sys.stdin.readlines()]
-    # print file_list
     if r.port() == 2222:
         linker = wikilinker.get_gitweb_linker(r)
+        commit_linker = wikilinker.get_gitweb_commit_linker(r)
     else:
         linker = wikilinker.get_cgit_linker(r)
+        commit_linker = wikilinker.get_cgit_commit_linker(r)
     if opts.template:
         print topReviewTemplate
     for path in file_list:
         link = linker.make_wiki_link(path)
         print link
     if opts.template:
+        if opts.branchfiles:
+            commits = r.branch_commits()
+        print "\n=Commits="
+        for commit in commits:
+            commit_msg = r.commit_message(commit)
+            print commit_linker.make_wiki_commit_link(commit,commit_msg)
         print bottomReviewTemplate
-    
+
 if __name__ == "__main__":
     # to debug:
     #pdb.runcall(main)
