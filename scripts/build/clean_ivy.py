e97ee45eb137a7f830a402a850ceb3cd992cfb3b
Ben Warfield <bwarfield@amplify.com>
2013-08-23 12:29:23 -0400

    Ivy repository cleaner refactor: axe some global variables.
    Another Fixer Friday contribution: remove two broadly-scoped lookup dictionaries and
    replace them with a better-encapsulated look at the same information (that information
    being the collection of builds for a given revision, the files associated with each build,
    and the timestamp for each build).
    
    Change-Id: Id3b252c31f99687bf3dc707ebce28190bed4b633

--- scripts/build/clean_ivy.py
+++ scripts/build/clean_ivy.py
@@ -19,6 +19,40 @@ dpp13numbers = "(?P<%s>\d+)\.(?P<%s>\d+)\.(?P<%s>\d+)-(?P<%s>\d+)" % (
 )
 dpp13format = "%s.%s.%s-%s"
 now = datetime.datetime.now()
+class patchlevel(object):
+	def __init__(self,dict=None,major=None,minor=None,patchlevel=None):
+		if dict:
+			self.major = dict[MAJOR]
+			self.minor = dict[MINOR]
+			self.patchlevel = dict[PATCHLEVEL]
+		else:
+			self.major = major
+			self.minor = minor
+			self.patchlevel = patchlevel
+		self.builds = []
+
+	def version_string(self):
+		return "%s.%s.%s" % (self.major, self.minor, self.patchlevel)
+
+	def add_build(self,build_number,timestamp):
+		self.builds.append({BUILD_NUMBER : int(build_number), "TIMESTAMP":timestamp, "FILES": []})
+
+	def get_build(self, build_number):
+		numeric_build_number = int(build_number)
+		for build in self.builds:
+			if build[BUILD_NUMBER] == numeric_build_number:
+				return build
+		raise Exception("Could not find build %s for patchlevel %s" % (build_number, self.version_string()))
+
+	def latest_build(self):
+		latest = self.builds[0]
+		for build in self.builds:
+			if build[BUILD_NUMBER] > latest[BUILD_NUMBER]:
+				latest = build
+		return latest
+
+	def build_count(self):
+		return len(self.builds)
 
 def get_build_list(h, keydict):
 	current_level = h
@@ -32,7 +66,7 @@ def get_build_list(h, keydict):
 		needsall = True
 	current_level = current_level[keydict[MINOR]]
 	if needsall or keydict[PATCHLEVEL] not in current_level:
-		current_level[keydict[PATCHLEVEL]] = []
+		current_level[keydict[PATCHLEVEL]] = patchlevel(keydict)
 	return current_level[keydict[PATCHLEVEL]]
 
 def main(module_name, build_to_dump=None):
@@ -43,16 +77,14 @@ def main(module_name, build_to_dump=None):
 	generalmatcher = re.compile("^[-\w]+-%s.[a-z]+$" % dpp13numbers)
 	badfiles = []
 	revision_map = {}
-	file_age = {}
 
 	for ivyfile in ivyfiles:
 		match = re.match(ivymatcher,ivyfile)
 		if match:
-			build_list = get_build_list(revision_map, match.groupdict())
-			build_list.append(int(match.group(BUILD_NUMBER)))
 			mtime = os.path.getmtime(os.path.join(root_dir, module_name, ivyfile))
 			filestamp = datetime.datetime.fromtimestamp(mtime)
-			file_age[ivyfile] = now - filestamp
+			build_list = get_build_list(revision_map, match.groupdict())
+			build_list.add_build(int(match.group(BUILD_NUMBER)), filestamp)
 		else:
 			badfiles.append(ivyfile)
 
@@ -61,33 +93,28 @@ def main(module_name, build_to_dump=None):
 		for minor_level_tuple in minor_revision_map.items():
 			(minor_revision, patchlevel_map) = minor_level_tuple
 			for patchlevel_tuple in patchlevel_map.items():
-				(patchlevel, builds) = patchlevel_tuple
-				latest_build = max(builds)
-				build_count = len(builds)
-				latest_age = file_age["ivy-%s.%s.%s-%s.xml" % (
-				    major_revision, minor_revision, patchlevel, latest_build
-				)]
-				print "Revision %s.%s.%s has %s builds (%s is latest, at %s days old)" % (
-				    major_revision, minor_revision, patchlevel,
-				    build_count, latest_build, latest_age.days
+				(patch_level, patchlevel_object) = patchlevel_tuple
+				latest_build = patchlevel_object.latest_build()
+				latest_age = now - latest_build["TIMESTAMP"]
+				print "Revision %s has %s builds (%s is latest, at %s days old)" % (
+				    patchlevel_object.version_string(),
+				    patchlevel_object.build_count(), latest_build[BUILD_NUMBER], latest_age.days
 				)
 
-	file_lookup = {}
 	for general_file in allfiles:
 		match = re.match(generalmatcher, general_file)
 		if match:
 			version = match.groupdict()
-			versionString = dpp13format % (version[MAJOR], version[MINOR],version[PATCHLEVEL], version[BUILD_NUMBER])
-			if versionString not in file_lookup:
-				file_lookup[versionString] = []
-			file_lookup[versionString].append(general_file)
+			build = get_build_list(revision_map, version).get_build(version[BUILD_NUMBER])
+			build["FILES"].append(general_file)
 
 	print "Non-matching files: "
 	print badfiles
 	if build_to_dump:
 		print "Dumping requested build files:"
-		versionString = dpp13format % (build_to_dump[0], build_to_dump[1], build_to_dump[2],build_to_dump[3])
-		for file_name in file_lookup[versionString]:
+		patchlevel = get_build_list(revision_map, {MAJOR:build_to_dump[0], MINOR:build_to_dump[1], PATCHLEVEL: build_to_dump[2]})
+		build = patchlevel.get_build(build_to_dump[3])
+		for file_name in build["FILES"]:
 			print file_name
 
 if __name__ == "__main__":
