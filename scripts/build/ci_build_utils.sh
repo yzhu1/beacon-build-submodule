f43f1d7f523308ee2f49a658d3243b24799a6bb3
Ben Warfield <bwarfield@amplify.com>
2013-09-20 14:12:19 -0400

    Refactored build utils to avoid duplicate work.
    Instead of building the RPM in the repository, build it locally and then copy to each repository
    in turn.  Probably slightly less I/O intensive, and definitely less weird than building it in
    place and then copying to another repository.
    
    Change-Id: I9a88661c569923285ae876579268138db9d8e89c

--- scripts/build/ci_build_utils.sh
+++ scripts/build/ci_build_utils.sh
@@ -19,24 +19,28 @@ function ci_build_utils.publish_rpms() {
     workspace=$5
     rpm_repositories=${@:6}
 
+    rpm_target_dir="$workspace/target/rpm"
+
     rm -rf $workspace/RPM_STAGING
     mkdir -p $workspace/opt/tt/webapps/$app
+    mkdir -p $rpm_target_dir
+    rm $rpm_target_dir/*
+    # Build webapp and db rpms in local workspace
+    python /opt/wgen/rpmtools/wg_rpmbuild.py -v -o $rpm_target_dir -r $workspace/RPM_STAGING \
+        -D${app}dir=$workspace -Drpm_version=$rpmversion -Dbuildnumber=$buildnumber $workspace/rpm/tt-$app.spec
+    python /opt/wgen/rpmtools/wg_rpmbuild.py -v -o $rpm_target_dir -r $workspace/RPM_STAGING \
+        -Dcheckoutroot=$workspace -Drpm_version=$rpmversion -Dbuildnumber=$buildnumber $workspace/rpm/tt-migrations-$app.spec
 
+    # not sure why this needs to exist, but no sense leaving it lying around
+    rm -rf $workspace/opt
+
+    # copy RPMs to each repository, and update
     for rpm_repo in $rpm_repositories
     do
         echo "building $app and $migrations RPMs for repository $rpm_repo"
         rm -f $rpm_repo/mclass-tt-$app-$rpmversion-*.noarch.rpm
         rm -f $rpm_repo/tt-migrations-$migrations-$rpmversion-*.noarch.rpm
-        # Build webapp and db rpms
-        python /opt/wgen/rpmtools/wg_rpmbuild.py -v -o $rpm_repo -r $workspace/RPM_STAGING \
-                -D${app}dir=$workspace -Drpm_version=$rpmversion -Dbuildnumber=$buildnumber $workspace/rpm/tt-$app.spec
-        python /opt/wgen/rpmtools/wg_rpmbuild.py -v -o $rpm_repo -r $workspace/RPM_STAGING \
-                -Dcheckoutroot=$workspace -Drpm_version=$rpmversion -Dbuildnumber=$buildnumber $workspace/rpm/tt-migrations-$app.spec
-    done
-    # Promote them to CI rpm repo
-    for rpm_repo in $rpm_repositories
-    do
-        echo "Running createrepo for $rpm_repo"
+        cp $rpm_target_dir/*.rpm $rpm_repo
         /opt/wgen/rpmtools/wg_createrepo $rpm_repo
     done
 }
