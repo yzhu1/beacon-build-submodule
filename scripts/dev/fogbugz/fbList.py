c1a48898e6f0b408b63dc91f55f3a1fd158b4374
Ankit Goswami <agoswami@contractor.wgen.net>
2013-12-14 02:49:25 -0500

    Updated fb script with much better ui and fixed bugs related to release option by making it simpler Change-Id: I93cba06af4c224c5b99350d4f41b86c32e8c28c5

--- scripts/dev/fogbugz/fbList.py
+++ scripts/dev/fogbugz/fbList.py
@@ -2,7 +2,7 @@
 import sys
 from fogbugz import FogBugz
 import optparse
-import fbSettings
+import fbConfigs
 import pickle
 import os
 import csv
@@ -22,7 +22,7 @@ def getargs():
     parser.add_option("-f", "--filter", dest="filter",action="store",help="apply the specified filter")
     parser.add_option("-r", "--release",action="store_true", dest="showReleaseInfo",help="gives additional info specific for release bugs (affects load times).")
     parser.add_option("-e", "--export",action="store_true", dest="export",help="exports results to csv")
-    parser.set_description('You need to have python and pip installed in order to use this script. \nAfter that run sudo pip install fogbugz. Now you can start using the script. \n If you don\'t specify any of the options, then a fogbugz search will be performed based on whatever argument you provide which can include a space or comma separated list of bug ids. If no arguments are provided, then the script will display bugs based on your currently active filter. Max number of bugs which can be listed have been limited to 999 but this can be changed in fbSettings.py')
+    parser.set_description('You need to have python and pip installed in order to use this script. \nAfter that run sudo pip install fogbugz. Now you can start using the script. \n If you don\'t specify any of the options, then a fogbugz search will be performed based on whatever argument you provide which can include a space or comma separated list of bug ids. If no arguments are provided, then the script will display bugs based on your currently active filter. Max number of bugs which can be listed have been limited to 999 but this can be changed in fbConfigs.py')
     parser.set_usage("%prog [options] [case ids ... ]")
     return parser.parse_args()
 
@@ -44,68 +44,73 @@ def build_query(opts, arglist):
 
 def getLatestBeaconReleaseEvent(events):
     for event in reversed(events):
-        if "FIX DESCRIPTION" in event.text:
+        if "STATUS" in event.text:
             return event
 
 def addBeaconReleaseProperties(case, caseProperties):
     events = case.findAll('event')
     latestReleaseEvent = getLatestBeaconReleaseEvent(events)
     if latestReleaseEvent :
-        beaconAttributesString = latestReleaseEvent.find('s').string
-        beaconAttributesArray = beaconAttributesString.split('\n')
-        defaultValue = "Not specified"
-        caseProperties['release_STATUS'] = defaultValue
-        caseProperties['release_MERGES'] = defaultValue
-        caseProperties['release_COMMIT'] = defaultValue
-        caseProperties['release_RISKS'] = defaultValue
-        caseProperties['release_FIXDESCRIPTION'] = defaultValue
-        caseProperties['release_BRANCH'] = defaultValue
-        caseProperties['release_PROJECT'] = ""
-        caseProperties['release_PROJECTS'] = ""
-        for beaconAttributes in beaconAttributesArray:
-            keyValueArray = beaconAttributes.split(':', 1)
-            if(len(keyValueArray) == 2):
-                key = keyValueArray[0].encode('ascii').replace(" ", "").replace("(S)", "")
-                value = keyValueArray[1].strip().replace("\r", "")
-                caseProperties["release_" + key] = value
+        caseProperties['release_info'] = latestReleaseEvent.find('s').string
 
 def find_cases(fb, query, showReleaseInfo):
     required_columns = "ixBug,sTitle,sStatus,sProject,sPersonAssignedTo,ixPriority,sFixFor"
     if showReleaseInfo:
         required_columns += ",events"
-    resp = fb.search(q=query,cols=required_columns,max=fbSettings.MAX_SEARCH_RESULTS)
+    resp = fb.search(q=query,cols=required_columns,max=fbConfigs.MAX_SEARCH_RESULTS)
     casePropertiesList = []
     cases = resp.cases.findAll('case')
     for case in cases:
         caseProperties = {}
         caseProperties['caseId'] = case.ixbug.string
         caseProperties['priority'] = case.ixpriority.string
-        caseProperties['status'] = case.sstatus.string.encode('utf-8')
-        caseProperties['project'] = case.sproject.string.encode('utf-8')
-        caseProperties['milestone'] = case.sfixfor.string.encode('utf-8')
-        caseProperties['assignedTo'] = case.spersonassignedto.string.encode('utf-8')
-        caseProperties['title'] = case.stitle.string.encode('utf-8')
+        caseProperties['status'] = getFixedLengthString(case.sstatus.string.encode('utf-8'), 20)
+        caseProperties['project'] = getFixedLengthString(case.sproject.string.encode('ascii', 'ignore'), 27)
+        caseProperties['milestone'] = getFixedLengthString(case.sfixfor.string.encode('utf-8'),20)
+        caseProperties['assignedTo'] = getFixedLengthString(case.spersonassignedto.string.encode('utf-8'), 13)
+        caseProperties['title'] = getFixedLengthString(case.stitle.string.encode('utf-8'), 50)
         if showReleaseInfo:
             addBeaconReleaseProperties(case, caseProperties)
         casePropertiesList.append(caseProperties)
     return casePropertiesList
 
-def printHLine():
+def getAvailableColumns():
     rows, columns = os.popen('stty size', 'r').read().split()
-    print '_' * int(columns) + "\n"
+    return int(columns)
+
+def printHLine():
+    print '-' * getAvailableColumns() + "\r"
+
+def getFixedLengthString(data, length):
+    if len(data) > length:
+        fixedLengthData = (data[:length-2] + '..')
+    elif len(data) < length:
+        fixedLengthData = data.ljust(length)
+    else:
+        fixedLengthData = data
+    return fixedLengthData
+
+def checkAndAddEllipsesToResultRow(data):
+    noFormattingStr = data.replace(fbConfigs.BLUE,"").replace(fbConfigs.GREEN,"").replace(fbConfigs.YELLOW,"").replace(fbConfigs.RED,"").replace(fbConfigs.ITALIC,"").replace(fbConfigs.UNDERLINE,"").replace(fbConfigs.MAGENTA,"").replace(fbConfigs.ENDC,"")
+    if len(noFormattingStr) > getAvailableColumns():
+        return (noFormattingStr[:getAvailableColumns()-2] + '..')
+    return data
+
 
 def show_cases_info(caseList, opts):
     print "Found " + str(len(caseList)) + " Matching case(s):"
     printHLine()
+    print getFixedLengthString(fbConfigs.HEADER, getAvailableColumns())
+    printHLine()
     for caseProperties in caseList:
-        print fbSettings.BUG_INFO_FORMAT.format(**caseProperties)
-        if opts.showReleaseInfo and 'release_STATUS' in caseProperties:
-            print fbSettings.RELEASE_INFO_FORMAT.format(**caseProperties)
+        print checkAndAddEllipsesToResultRow(fbConfigs.BUG_INFO_FORMAT.format(**caseProperties)) + fbConfigs.ENDC
+        if opts.showReleaseInfo and 'release_info' in caseProperties:
+            print caseProperties['release_info']
         printHLine()
 
 def export_case_info(caseList, filename, hasReleaseInfo):
     f = open(filename, 'wb')
-    print "Exporting results to " + fbSettings.YELLOW + os.path.abspath(filename) + fbSettings.ENDC
+    print "Exporting results to " + fbConfigs.YELLOW + os.path.abspath(filename) + fbConfigs.ENDC
     keys = ['caseId', 'priority', 'status', 'project', 'milestone', 'assignedTo', 'title']
     if hasReleaseInfo:
         keys += ['release_STATUS', 'release_MERGES', 'release_PROJECT', 'release_RISKS', 'release_PROJECTS', 'release_FIXDESCRIPTION', 'release_BRANCH', 'release_COMMIT']
@@ -120,13 +125,13 @@ def setFilter(fb, filterString):
     matchingFilter = ''
     for filter in filters:
         if filterString == filter.string or filterString == filter['sfilter']:
-            print fbSettings.BLUE + "Applying new filter: " + filter['sfilter'] + " " + filter.string + fbSettings.ENDC
+            print fbConfigs.BLUE + "Applying new filter: " + filter['sfilter'] + " " + filter.string + fbConfigs.ENDC
             matchingFilter = filter['sfilter']
             break
     if len(matchingFilter) > 0:
         fb.setCurrentFilter(sFilter=matchingFilter)
     else:
-        print fbSettings.RED + "No filter found for: " + filterString + fbSettings.ENDC
+        print fbConfigs.RED + "No filter found for: " + filterString + fbConfigs.ENDC
         printAllFilters(fb)
 
 def getCurrentFilter(fb):
@@ -149,7 +154,7 @@ def getAuthToken():
     params = {'cmd':'logon'}
     params['email'] = email
     params['password'] = password
-    response = requests.post(fbSettings.AUTH_URL, params)
+    response = requests.post(fbConfigs.AUTH_URL, params)
     root = ET.fromstring(response.text)
     return root.findall('token')[0].text
 
@@ -162,14 +167,14 @@ def main(argv):
         authToken = getAuthToken()
         pickle.dump(authToken, open(tokenFilename,"wb"))
     try:
-        fb = FogBugz(fbSettings.URL, authToken)
+        fb = FogBugz(fbConfigs.URL, authToken)
         currentFilter = getCurrentFilter(fb)
     except:
         print "Could not authenticate" + str(authToken)
         os.remove(tokenFilename)
         main(argv)
     if currentFilter and len(argv) == 0:
-        print fbSettings.YELLOW + "Active filter: " + currentFilter.string + fbSettings.ENDC
+        print fbConfigs.YELLOW + "Active filter: " + currentFilter.string + fbConfigs.ENDC
     if opts.filter:
         setFilter(fb, opts.filter)
     query = build_query(opts, arglist)
