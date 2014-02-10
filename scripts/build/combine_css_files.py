2f7e63f6d1c425c4c4ab23a007c9017e992f7f8f
Yassine Ennaji <yennaji@amplify.com>
2014-02-10 11:41:24 -0500

    Revert "US20639 - Python versions before 2.6 don't support os.path.relPath. Updated script with an implementation of relPath."
    This reverts commit b6914ca9a9448583dc1c12b904a9c5ffad7f36b3.
    
    Change-Id: I712183edd2a7723a9c101d21dc1669c3ec1c3e4d

--- scripts/build/combine_css_files.py
+++ scripts/build/combine_css_files.py
@@ -1,22 +1,7 @@
 #!/usr/bin/python
 
-import sys, os
-from posixpath import curdir, sep, pardir, join, abspath, commonprefix
-
-# Using this implementation of os.path.relpath because it does not exist in python 2.5, that is used by our build slaves.
-# This function is copied from http://www.saltycrane.com/blog/2010/03/ospathrelpath-source-code-python-25/
-def relpath(path, start=curdir):
-    """Return a relative version of a path"""
-    if not path:
-        raise ValueError("no path specified")
-    start_list = abspath(start).split(sep)
-    path_list = abspath(path).split(sep)
-    # Work out how much of the filepath is shared by start and path.
-    i = len(commonprefix([start_list, path_list]))
-    rel_list = [pardir] * (len(start_list)-i) + path_list[i:]
-    if not rel_list:
-        return curdir
-    return join(*rel_list)
+import sys
+import os
 
 cssDir = sys.argv[1] + '/src/main/webapp/static/css'
 tabletDirName = 'tablet/'
@@ -33,7 +18,7 @@ except OSError:
 fileSet = set()
 for dir_, _, files in os.walk(cssDir):
     for filename in files:
-        relDir = relpath(dir_, cssDir)
+        relDir = os.path.relpath(dir_, cssDir)
         relFile = os.path.join(relDir, filename)
         fileSet.add(relFile)
 
