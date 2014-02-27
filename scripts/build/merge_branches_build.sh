56b68dd784f349787988884774639e9926fbe688
Yassine Ennaji <yennaji@amplify.com>
2014-02-27 11:06:04 -0500

    US22927 - Update the merge_branches script to force pushes.
    Change-Id: Ie1246571fe04ba0bc09eaa541a73078eda8c1f23

--- scripts/build/merge_branches_build.sh
+++ scripts/build/merge_branches_build.sh
@@ -8,6 +8,6 @@ headbranch=$2
 basebranch=$3
 
 git fetch $gitrepobaseurlgit/$gitrepo
-git checkout $basebranch
+git checkout origin-$gitrepo $basebranch
 git merge origin-$gitrepo/$headbranch
-git push $gitrepobaseurlgit/$gitrepo $basebranch
+git push -f origin-$gitrepo $basebranch 
