e8af5bc147af8e6544c037f051f8ff6daaab2805
Yassine Ennaji <yennaji@amplify.com>
2014-02-27 15:38:15 -0500

    US22927 - branch merging script - force failure if commands fail.
    Change-Id: Ie67a614fca7e0d0b3bd133a1c356a323c8188142

--- scripts/build/merge_branches_build.sh
+++ scripts/build/merge_branches_build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Script for up-stream merges. (i.e release->master or master->next)
+# Script for up-stream merges. (i.e release->master, master->next)
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
 
 gitrepobaseurl="git@github.wgenhq.net:Beacon"
 gitrepo=$1
@@ -8,16 +11,16 @@ headbranch=$2
 basebranch=$3
 
 echo "** Reset: $basebranch"
-git fetch $gitrepobaseurlgit/$gitrepo
-git reset --hard origin-$gitrepo/$basebranch
+git fetch origin
+git reset --hard origin/$basebranch
 
 echo "** Checkout: $basebranch"
-git checkout origin-$gitrepo $basebranch
+git checkout origin/$basebranch
 echo "** Pull: $basebranch"
-git pull origin-$gitrepo $basebranch
+git pull origin $basebranch
 
 echo "** Merge $headbranch into $basebranch"
-git merge origin-$gitrepo/$headbranch
+git merge origin/$headbranch
 
 echo "** Push merge commits to $basebranch"
-git push origin-$gitrepo $basebranch
+git push origin $basebranch
